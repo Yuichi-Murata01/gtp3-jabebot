9fe0f77e08d3ede8794278f9bc4157ec577f4e68
Yuichi-Murata01 <yuichi121201@gmail.com>
2022-04-15 18:10:31 +0900

    seconde push

--- jabebot.py
+++ jabebot.py
@@ -1,10 +1,9 @@
-
-
 from dotenv import load_dotenv
 from random import choice
 from flask import Flask, request
 import os
 import openai
+
 
 load_dotenv()
 #openai.api_key = os.getenv("OPENAI_API_KEY")
