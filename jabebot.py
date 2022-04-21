f5524c11eeb987c6ae3eb1bb272d211db465093b
Yuichi-Murata01 <yuichi121201@gmail.com>
2022-04-21 16:36:35 +0900

    update

--- jabebot.py
+++ jabebot.py
@@ -6,8 +6,7 @@ import openai
 
 
 load_dotenv()
-#openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key = "your-openai-api-key-here"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 completion = openai.Completion()
 
 start_sequence = "\nJabe:"
